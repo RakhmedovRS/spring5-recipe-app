3e6b8d4b37aa482c794b20ec8da6fdc02015422d
rassoll <rassoll32@gmail.com>
2020-05-18 22:48:25 +0300

    Created 2 entities

---
+++ src/main/java/com/github/rakhmedovrs/domain/Notes.java
@@ -0,0 +1,51 @@
+package com.github.rakhmedovrs.domain;
+
+import javax.persistence.*;
+
+/**
+ * @author RakhmedovRS
+ * @created 18-May-20
+ */
+@Entity
+public class Notes
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@OneToOne
+	private Recipe recipe;
+
+	@Lob
+	private String recipeNotes;
+
+	public Long getId()
+	{
+		return id;
+	}
+
+	public void setId(Long id)
+	{
+		this.id = id;
+	}
+
+	public Recipe getRecipe()
+	{
+		return recipe;
+	}
+
+	public void setRecipe(Recipe recipe)
+	{
+		this.recipe = recipe;
+	}
+
+	public String getRecipeNotes()
+	{
+		return recipeNotes;
+	}
+
+	public void setRecipeNotes(String recipeNotes)
+	{
+		this.recipeNotes = recipeNotes;
+	}
+}
