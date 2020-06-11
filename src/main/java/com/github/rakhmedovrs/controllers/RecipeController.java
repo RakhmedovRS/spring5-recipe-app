558bd031dbcbd63c999724763976ebaccb24680f
rassoll <rassoll32@gmail.com>
2020-06-11 21:36:22 +0300

    Added Data Validation

--- src/main/java/com/github/rakhmedovrs/controllers/RecipeController.java
+++ src/main/java/com/github/rakhmedovrs/controllers/RecipeController.java
@@ -7,8 +7,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
 
 /**
  * @author RakhmedovRS
@@ -18,6 +21,8 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class RecipeController
 {
+	private final static String RECIPE_FORM_URL = "recipe/recipeform";
+
 	private final RecipeService recipeService;
 
 	public RecipeController(RecipeService recipeService)
@@ -36,19 +41,26 @@ public class RecipeController
 	public String newRecipe(Model model)
 	{
 		model.addAttribute("recipe", new RecipeCommand());
-		return "recipe/recipeform";
+		return RECIPE_FORM_URL;
 	}
 
 	@GetMapping("recipe/{id}/update")
 	public String updateRecipe(@PathVariable String id, Model model)
 	{
 		model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
-		return "recipe/recipeform";
+		return RECIPE_FORM_URL;
 	}
 
 	@PostMapping("recipe")
-	public String saveOrUpdate(@ModelAttribute RecipeCommand recipeCommand)
+	public String saveOrUpdate(@Valid @ModelAttribute("recipe") RecipeCommand recipeCommand,
+	                           BindingResult bindingResult)
 	{
+		if (bindingResult.hasErrors())
+		{
+			bindingResult.getAllErrors().forEach(object -> log.debug(object.toString()));
+			return RECIPE_FORM_URL;
+		}
+
 		RecipeCommand savedCommand = recipeService.saveRecipeCommand(recipeCommand);
 		return "redirect:/recipe/" + savedCommand.getId() + "/show";
 	}
